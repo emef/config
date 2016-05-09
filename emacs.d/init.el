d2a16c7ac5fe421d7aa31234ed13dc0b2bd33f61
Matt Forbes <mforbes@twitter.com>
2016-05-08 17:10:46 -0700

    update

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -1,30 +1,36 @@
-(when window-system
-  (scroll-bar-mode 0)
-  (setq mac-command-modifier 'meta))
-
+;; augment path and use common lisp features
 (setenv "PATH" (concat "/usr/local/bin:" (getenv "PATH")))
+(setenv "PATH" (concat "/opt/local/bin:" (getenv "PATH")))
+(setq exec-path (append exec-path '("/usr/local/gnat/bin/"
+                                    "/usr/local/bin"
+                                    "/usr/texbin"
+                                    "/opt/local/bin") exec-path))
 (require 'cl)
 
 ;; setup load path
-(push (expand-file-name "~/.emacs.d") load-path)
+(push (expand-file-name "~/.emacs.d/matt") load-path)
+(push (expand-file-name "~/.emacs.d/async") load-path)
+(push (expand-file-name "~/.emacs.d/helm") load-path)
 
+;; get our packages up and running
 (load "package")
 (package-initialize)
 
 (add-to-list 'package-archives
              '("marmalade" . "http://marmalade-repo.org/packages/"))
 (add-to-list 'package-archives
-             '("melpa" . "http://melpa.milkbox.net/packages/") t)
-
+             '("melpa" . "http://melpa.milkbox.net/packages/"))
 
 (defvar matt/packages '(auto-complete
 			autopair
-			gist
 			go-mode
 			htmlize
 			org
 			paredit
-			color-theme)
+			color-theme
+      projectile
+      flx
+      flx-ido)
   "Default packages")
 
 (defun matt/packages-installed-p ()
@@ -39,13 +45,28 @@
     (when (not (package-installed-p pkg))
       (package-install pkg))))
 
-(require 'color-theme)
+(require 'ido)
 (require 'my-keys)
 (require 'my-ido)
+(require 'my-linum)
+(require 'color-theme)
 (require 'uniquify)
 (require 'auto-complete)
-(require 'ido)
 (require 'python)
+(require 'helm-config)
+(require 'google-c-style)
+
+(when window-system
+  (scroll-bar-mode 0)
+  (setq mac-command-modifier 'meta)
+  (color-theme-solarized-dark))
+
+;; tags
+(setq tags-table-list
+      '("~/.tags/tellapart"))
+
+;; projectile
+(projectile-global-mode)
 
 ;; autocomplete
 (global-auto-complete-mode t)
@@ -73,6 +94,7 @@
 
 ;; line numbers
 (global-linum-mode t)
+(setq linum-format "%d ")
 
 ;; shitty whitespace
 (add-hook 'before-save-hook 'delete-trailing-whitespace)
@@ -80,19 +102,48 @@
 ;; Highlight matching parentheses.
 (show-paren-mode t)
 
+;; c/c++ style
+;(add-hook 'c-mode-common-hook 'google-set-c-style)
+;(add-hook 'c-mode-common-hook 'google-make-newline-indent)
+;(add-to-list 'auto-mode-alist '("\\.h\\'" . c++-mode))
+(add-to-list 'auto-mode-alist '("\\.cc\\'" . c++-mode))
+
+(defconst my-cc-style
+  '("gnu"
+    (c-offsets-alist . ((innamespace . [0])))))
+(c-add-style "my-cc-mode" my-cc-style)
+
 ;;; general settings
 (setq-default c-default-style "linux"
-              tab-width 4
-              default-tab-width 4
+              tab-width 2
+              default-tab-width 2
               c-basic-offset 4
-              indent-tabs-mode nil)
+              indent-tabs-mode nil
+              js-indent-level 2)
+
+(helm-mode 1)
+(define-key global-map [remap find-file] 'helm-find-files)
+(define-key global-map [remap occur] 'helm-occur)
+(define-key global-map [remap list-buffers] 'helm-buffers-list)
+(define-key global-map [remap dabbrev-expand] 'helm-dabbrev)
+(global-set-key (kbd "M-x") 'helm-M-x)
+(unless (boundp 'completion-in-region-function)
+  (define-key lisp-interaction-mode-map [remap completion-at-point] 'helm-lisp-completion-at-point)
+  (define-key emacs-lisp-mode-map       [remap completion-at-point] 'helm-lisp-completion-at-point))
+
+(setq ggtags-enable-navigation-keys nil)
+(eval-after-load "ggtags-mode"
+  '(progn
+     (define-key ggtags-mode-keymap (kbd "M-n") nil)
+     (define-key ggtags-mode-keymap (kbd "\M-.") nil)))
+
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
-; '(ansi-color-names-vector ["#2e3436" "#a40000" "#4e9a06" "#c4a000" "#204a87" "#5c3566" "#729fcf" "#eeeeec"])
+ '(ansi-color-names-vector ["#2e3436" "#a40000" "#4e9a06" "#c4a000" "#204a87" "#5c3566" "#729fcf" "#eeeeec"])
  '(comint-input-ignoredups t)
  '(comint-input-ring-size 500)
  '(custom-safe-themes (quote ("e7b8656484a079b3651360c621be0cf2bb6d67a2b9cf33b454cfc4d55fb22e58" "9a290da044d7847abab3502b98e3849284e4ee93bc9ffff7bb0af0967f9226cc" "a5057d754b6482dd48e3fc099004026c75592794c322bc7368368aaaf332897c" "1d13b4ccd3b56e6264f5895b5c6c575eedbfb73a36aaeb05b91565ea211faa2f" "ebdf5574672e641900e6c16275d0594b7de5b95d8e1283f57f9144a4eebf7806" "dc758223066a28f3c6ef6c42c9136bf4c913ec6d3b710794252dc072a3b92b14" default)))
@@ -100,9 +151,6 @@
  '(fringe-mode 0 nil (fringe))
  '(tool-bar-mode nil)
  '(yas/prompt-functions (quote (yas/dropdown-prompt yas/ido-prompt))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+(put 'erase-buffer 'disabled nil)
+(put 'downcase-region 'disabled nil)
+(put 'upcase-region 'disabled nil)
--- emacs.d/matt/my-ido.el
+++ emacs.d/matt/my-ido.el
@@ -1,5 +1,12 @@
 ;; ido
+(require 'ido)
+(require 'flx)
+(require 'flx-ido)
+
 (ido-mode t)
+(ido-everywhere 1)
+(flx-ido-mode 1)
+
 (setq ido-enable-prefix nil
       ido-enable-flex-matching t
       ido-auto-merge-work-directories-length nil
@@ -8,6 +15,7 @@
       ido-use-virtual-buffers t
       ido-handle-duplicate-virtual-buffers 2
       ido-max-prospects 10
+      ido-use-faces nil
       ido-use-filename-at-point nil)
 
 ;; Display ido results vertically, rather than horizontally
@@ -16,3 +24,6 @@
 (add-hook 'ido-minibuffer-setup-hook 'ido-disable-line-trucation)
 
 (provide 'my-ido)
+
+
+(ido-mode 1)
